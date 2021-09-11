5cb5022349c21e44e2621a795e74743bda0007c3
Quincy Stafford <quincy.stafford@yahoo.com>
2021-09-11 10:25:09 -0500

    added document and for loop

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 // X is always first
 //if there is a winner or a draw use an alert
 //clicking the board when the game is over will reset the board
+
+let cells = document.querySelectorAll('.row > div')
+
+for (let i = 0; i < cells.length; i++) {
+}
