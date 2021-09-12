17c68b737876e5e6350a2646ae1d5545fb5794cd
Quincy Stafford <quincy.stafford@yahoo.com>
2021-09-12 07:35:07 -0500

    restarted

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // X is always first
 //if there is a winner or a draw use an alert
 //clicking the board when the game is over will reset the board
+/*ideas
+define player_1 and player_2
+when clicked determines player1 and 2
+changeTurn function
+winner function
 
+*/
 let cells = document.querySelectorAll('.row > div')
 
 for (let i = 0; i < cells.length; i++) {
@@ -11,3 +17,4 @@ for (let i = 0; i < cells.length; i++) {
 function cellClicked () {
     event.target.textContent = 'X'
 }
+
