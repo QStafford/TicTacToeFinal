0bfd951e1ac37cdba457386ce1f6f5b733eed41e
Quincy Stafford <quincy.stafford@yahoo.com>
2021-09-12 08:27:39 -0500

    added plaer1 and 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ define player_1 and player_2
 when clicked determines player1 and 2
 changeTurn function
 winner function
-
+after game is over click to reset
 */
 let cells = document.querySelectorAll('.row > div')
 
@@ -15,6 +15,33 @@ for (let i = 0; i < cells.length; i++) {
 }
 
 function cellClicked () {
-    event.target.textContent = 'X'
+    event.target.textContent = player_1
 }
 
+let player_1 = 'X'
+let player_2 = 'O'
+let turn = true
+/*
+function changeTurn (){
+    if (event.textContent = player_1) {
+        return(event.textContent = player_2)
+        
+    }
+    else{
+        event.textContent = player_1
+    }
+}
+*/
+function playerTurn() {
+    if (textContent = player_1) {
+         event.textContent = player_2
+    }
+}
+
+function chooseWinner(){
+
+}
+
+function reset(){
+
+}
